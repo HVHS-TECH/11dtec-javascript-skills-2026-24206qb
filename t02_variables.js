92e828b93ee5b14996f8417316cd87feb410a256
Quoc Bao <24206qb@hvhs.school.nz>
2026-04-23 02:57:14 +0000

    Apr 23, 2026, 2:57 AM

--- t02_variables.js
+++ t02_variables.js
@@ -5,8 +5,8 @@ Variables
 console.log("Dino world is running")
 
 
-let name: QuocBao;
-let Years: 2026;
+let name = "QuocBao"
+let Years = 2026;
 let name: QuocBao;
 let name: QuocBao;
 console.log(Dino world is running)
