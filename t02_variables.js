4db4e982525bedb3b1fc9f7fe7048cf2efd2e03d
Quoc Bao <24206qb@hvhs.school.nz>
2026-04-23 03:05:19 +0000

    Apr 23, 2026, 3:05 AM

--- t02_variables.js
+++ t02_variables.js
@@ -2,16 +2,16 @@
 Variables
 ****************************/
 
-console.log("Dino world is running")
+console.log("Running t02_variables.js")
 
 
-let name = "QuocBao"
+let name = " QuocBao"
 let Years = 2026;
-let name: QuocBao;
-let name: QuocBao;
-console.log(Dino world is running)
-console.log(Dino world is running)
-console.log(Dino world is running)
+let Yearold = 15;
+let money = 10;
+console.log("Hi" + name);
+console.log("As of " + Years + " you are " + Yearold + " Years old ");
+console.log("You have " + money + " dollars");
 // Variable
 
 /****************************
