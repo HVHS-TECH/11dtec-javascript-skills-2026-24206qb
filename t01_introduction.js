cc06b0459a1a48dcd78ec3964dd1bb1ceaf81ef3
Quoc Bao <24206qb@hvhs.school.nz>
2026-04-23 02:37:58 +0000

    Apr 23, 2026, 2:37 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,14 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+introduction
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
 
-// This is a single line comment
+// Variable
 
+/****************************
+Dino World header
+****************************/
+
+/****************************
+Footer
+****************************/
