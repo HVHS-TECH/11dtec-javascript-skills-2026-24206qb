685ad872a008190e61ac632a30df3f7e55c534c9
Quoc Bao <24206qb@hvhs.school.nz>
2026-04-23 03:07:25 +0000

    Apr 23, 2026, 3:07 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 2 here */
+/****************************
+Maths.js
+****************************/
+
+console.log("Running t03_maths.js")
+
+
+let name = " QuocBao"
+let Years = 2026;
+let Yearold = 15;
+let money = 10;
+console.log("Hi" + name);
+console.log("As of " + Years + " you are " + Yearold + " Years old ");
+console.log("You have " + money + " dollars");
+// Variable
+
+/****************************
+Dino World header
+****************************/
+
+/****************************
+Footer
+****************************/
