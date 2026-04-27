3fcfea1f21496df7e5633cba605ff190a64cda47
Quoc Bao <24206qb@hvhs.school.nz>
2026-04-27 22:59:20 +0000

    Done task 3 & 4

--- t03_maths.js
+++ t03_maths.js
@@ -6,12 +6,23 @@ console.log("Running t03_maths.js")
 
 
 let name = " QuocBao"
-let Years = 2026;
-let Yearold = 15;
+let Year = 2026;
 let money = 10;
+let Yearold = 16;
+
 console.log("Hi" + name);
-console.log("As of " + Years + " you are " + Yearold + " Years old ");
+
+console.log("As of " + Year + " you are " + Yearold + " Years old ");
+born = Year - Yearold;
+console.log("You were born in " + born);
+Years = Yearold + 10;
+console.log("In 10 years you will be " + Years+ " years old");
+
 console.log("You have " + money + " dollars");
+money = money / 2;
+console.log("You spend half of your money, now you have " + money);
+money = money + 3;
+console.log("Then you get $3, now you have " + money);
 // Variable
 
 /****************************
