e3143946d822069dfaf7c1dd2d7c68ed4daea222
Quoc Bao <24206qb@hvhs.school.nz>
2026-05-10 21:56:00 +0000

    sws

--- t11_data_types.js
+++ t11_data_types.js
@@ -5,6 +5,14 @@ console.log("Running t11_data_types.js")
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
+
+/****************************
+Dino World header
+****************************/
+
+
+function showOutput(username, userAges, usermoney) {
+
 let Year = 2026;
 let money = 10;
 let Yearold = 16;
@@ -20,25 +28,15 @@ function displayProduct(_name, _price){
 OUTPUT.innerHTML += "<p>"+ _name  + _price + "</p>";
 }
 
-/****************************
-Dino World header
-****************************/
-
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>"
-
-function showOutput(username) {
-
 OUTPUT.innerHTML = "<p> Hi " + username + "</p>";
 
-}
-
-OUTPUT.innerHTML += "<p>As of " + Year + " you are " + Yearold + " Years old</p>";
-born = Year - Yearold;
+OUTPUT.innerHTML += "<p>As of " + Year + " you are " + userAges + " Years old</p>";
+born = Year - userAges;
 OUTPUT.innerHTML += "<p> You were born in " + born + "</p>";
-Years = Yearold + 10;
+Years = userAges + 10;
 OUTPUT.innerHTML += "<p>In 10 years you will be " + Years+ " years old </p>";
 
-OUTPUT.innerHTML += "<p> You have " + money + " dollars</p>";
+OUTPUT.innerHTML += "<p> You have " + usermoney + " dollars</p>";
 money = money / 2;
 OUTPUT.innerHTML += "<p> You spend half of your money, now you have " + money + "</p>";
 money = money + 3;
@@ -50,6 +48,7 @@ displayProduct("Chocolate bar: ", "$4")
 displayProduct("Chips: ", "$3")
 displayProduct("Drink: ", "$2.50")
 
+}
 
 /****************************
 Footer
